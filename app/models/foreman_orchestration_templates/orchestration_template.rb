3d227fd934c085d8f1ae1a78a2cd699950d102b5
Tomas Strachota <tstrachota@redhat.com>
2017-01-31 15:05:41 +0100

    Support for importing via foreman_templates

--- app/models/foreman_orchestration_templates/orchestration_template.rb
+++ app/models/foreman_orchestration_templates/orchestration_template.rb
@@ -28,5 +28,27 @@ module ForemanOrchestrationTemplates
     def job_configurations
       configurations.where.not(:template_id => nil)
     end
+
+    def self.import!(name, text, metadata)
+      current = self.find_by_name(name)
+      if current.nil?
+        template = self.new(:name => name, :template => text)
+        return {
+          :status => template.save!,
+          :result => "Template '#{name}' created",
+          :old => "",
+          :new => text
+        }
+      else
+        old_text = current.template.dup
+        current.template = text
+        return {
+          :status => current.save!,
+          :result => "Template '#{name}' updated",
+          :old => old_text,
+          :new => text
+        }
+      end
+    end
   end
 end
